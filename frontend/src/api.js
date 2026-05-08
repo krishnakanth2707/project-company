4159967236536d7f272874c60c2d78d6e527b245
krishnakanth2707 <krishnabalaji50@gmail.com>
2026-05-08 09:58:31 +0530

    Delete frontend directory

--- frontend/src/api.js
+++
@@ -1,5 +0,0 @@
-import axios from "axios";
-const BASE = "http://localhost:8000";
-
-export const analyzeStock = (ticker, period) =>
-  axios.post(`${BASE}/analyze`, { ticker, period }).then(r => r.data);
